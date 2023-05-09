56126c7f6cacc4576b5046fe62a7a8371c9e83f3
katson95 <katson95@gmail.com>
2023-05-09 06:28:24 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,44 @@
 package main
 
-import "github.com/amba-p2p/internal/platform/k8s"
+import (
+	"github.com/amba-p2p/internal/platform/db/cassandra"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
 	//node.Run()
 
-	k8s.Run()
-}
+	//k8s.Run()
 
+	installer := cassandra.New()
+
+	err := installer.Update()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = installer.PreReqJDK()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = installer.AddRepository()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = installer.AddKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = installer.Install()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = installer.Status()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
